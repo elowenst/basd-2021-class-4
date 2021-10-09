4910591ea82647c3d0bed3869ea579fb976295d1
Eric Lowenstein <lowenstein.eric@gmail.com>
2021-10-09 11:28:46 -0300

    added if else js file

---
+++ exercises/04_if_else.js
@@ -0,0 +1,26 @@
+// If Else
+var num = Math.random();
+console.log(num);
+if(num >= 0.5){
+    alert("Greater than 0,5");
+} else {
+    alert("Lower than 0,5");
+};
+
+var Age = 20
+if (Age < 2){
+    console.log("Bebe");
+} else if (Age >= 2 && Age <= 12){
+    console.log("Nino");
+} else if (Age >= 13 && Age <= 19){
+    console.log("Adolescente");
+} else if (Age >= 20 && Age <= 30){
+    console.log("Joven")
+} else if (Age >= 31 && Age <= 60){
+    console.log("Adulto");
+} else if (Age >= 61 && Age <= 75){
+    console.log("Adulto mayor");
+} else if (Age > 75){
+    console.log("Anciano");
+};
+
