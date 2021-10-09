1e70ae710686b6b7f77a6def702ca1c2f13e8838
Eric Lowenstein <lowenstein.eric@gmail.com>
2021-10-09 09:49:30 -0300

    added variables and operators js file

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,16 @@
+// Variables y Operadores
+var num1 = 1;
+var num2 = 2;
+var sum = num1 + num2;
+console.log(sum);
+
+string1 = "Soft";
+string2 = "ware";
+string3 = string1 + string2;
+console.log(string3);
+
+string4 = "Eric";
+string5 = "Lowenstein";
+sumString = string4.length + string5.length;
+console.log(sumString);
+
